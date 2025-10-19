00e8a10ca52b6c2332b70238efe478032839a725
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2025-10-19 19:48:43 -0300

    Adicionando funcionalidades a página

---
+++ script.js
@@ -0,0 +1,75 @@
+
+const imagemVisualizacao = document.querySelector("#visualizacao img");
+const tituloProduto = document.querySelector("h1");
+const nomeCorSelecionada = document.querySelector("#nome-cor-selecionada");
+const imagemMiniatura0 = document.querySelector('[for="0-imagem"] img');
+const imagemMiniatura1 = document.querySelector('[for="1-imagem"] img');
+const imagemMiniatura2 = document.querySelector('[for="2-imagem"] img');
+// console.log(imagemMiniatura0);
+
+const opcoesTamanho = ["41 mm", "45 mm"];
+const opcoesCores = [
+    "Verde-cipreste",
+    "Azul-inverno",
+    "Meia-noite",
+    "Estelar",
+    "Rosa-Claro"
+];
+
+// console.log(opcoesTamanho[1]);
+
+let numImagemSelecionada = 1;
+let numTamanhoSelecionado = 1;
+let numCorSelecionada = 1;
+
+function atualizarCorSelecionada() {
+     const opcaoCorSelecionada = document.querySelector('[name="opcao-cor"]:checked').id.charAt(0); // 0, 1, 2, 3 ou 4
+
+     numCorSelecionada = opcaoCorSelecionada
+
+     const nomeCor = opcoesCores[numCorSelecionada];
+
+     console.log(nomeCor);
+}
+
+
+function atualizarTamanho() {
+    const opcaoTamanhoSelecionado = document.querySelector(
+        '[name = "opcao-tamanho"]:checked').id.charAt(0) // 0 ou 1 - 41 mm ou 45 mm
+        
+        numTamanhoSelecionado = opcaoTamanhoSelecionado;
+
+        // console.log(opcoesTamanho[numTamanhoSelecionado]);
+
+        const tamanhoCaixa = opcoesTamanho[numTamanhoSelecionado];
+        
+        // console.log(opcaoTamanhoSelecionado);
+
+
+        tituloProduto.innerText = 
+        "Pulseira loop esportiva azul-inverno para caixa de " + tamanhoCaixa;
+
+        if(tamanhoCaixa === "41 mm") {
+            
+            imagemVisualizacao.classList.add('caixa-pequena');
+        } else {
+            
+            imagemVisualizacao.classList.remove('caixa-pequena');
+        }
+    }
+
+function atualizarImagemSelecionada() {
+
+    const opcaoImagemSelecionada = document.querySelector(
+        '[name= "opcao-imagem"]:checked'
+    ).id.charAt(0);
+
+    numImagemSelecionada = opcaoImagemSelecionada;
+
+    // console.log(opcaoImagemSelecionada);
+
+    imagemVisualizacao.src = 
+    "./imagens/opcoes-cores/imagens-azul-inverno/imagem-" + numImagemSelecionada +".jpeg";
+}
+
+// atualizarImagemSelecionada();
